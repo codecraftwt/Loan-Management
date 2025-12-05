b7b4040973c84d29ef051fba361bf21679b7cb03
codecraftwt <codecraftwt@gmail.com>
2025-12-05 11:38:53 +0530

    ios chnages

--- Loan-frontend/react-native.config.js
+++ Loan-frontend/react-native.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-module.exports={
-    project:{
-        ios:{},
-        android:{}
-    },
-    assets:['./src/Assets/fonts']
-=======
 module.exports = {
   project: {
     ios: {},
     android: {},
   },
   assets: ['./src/Assets/fonts'],
->>>>>>> c0261f0ed1e04f102b1bbed4398e1dd71230c3f2
 };
